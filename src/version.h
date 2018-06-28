987db782df92f6548b5dc8956fb50060c6dfc36d
Mike <zpoolsde@gmail.com>
2018-06-28 12:24:18 +0200

    version

--- src/version.h
+++ src/version.h
@@ -30,17 +30,17 @@ static const int DATABASE_VERSION = 70510;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 70013;
+static const int PROTOCOL_VERSION = 70014;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70011;
+static const int MIN_PEER_PROTO_VERSION = 70014;
 
-static const int MIN_INSTANTX_PROTO_VERSION = 70011;
+static const int MIN_INSTANTX_PROTO_VERSION = 70014;
 
-static const int MIN_MN_PROTO_VERSION = 70011;
+static const int MIN_MN_PROTO_VERSION = 70014;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
